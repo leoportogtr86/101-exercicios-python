64b22139120ebd63b37cd0185149ac8ebfcd030e
Leo Porto <leonardoreisporto@gmail.com>
2024-08-17 09:31:55 -0300

    exercicios

---
+++ Básico/ex2.py
@@ -0,0 +1,22 @@
+# 2. **Operações Matemáticas Simples**: Calcule a soma, subtração, multiplicação e
+# divisão de dois números.
+def soma(a, b):
+    return a + b
+
+
+def subtracao(a, b):
+    return a - b
+
+
+def divisao(a, b):
+    return a / b
+
+
+def multiplicacao(a, b):
+    return a * b
+
+
+print(soma(10, 5))
+print(subtracao(10, 5))
+print(divisao(10, 5))
+print(multiplicacao(10, 5))
---
+++ Básico/ex3.py
@@ -0,0 +1,7 @@
+# 3. **Cálculo de Área de um Retângulo**: Peça ao usuário a largura e a altura, e calcule a área.
+print("**************** Vamos calcular a área de um triângulo ****************")
+base = int(input("Digite o valor da base: "))
+altura = int(input("Digite o valor da altura: "))
+area = (base * altura) / 2
+
+print(area)
