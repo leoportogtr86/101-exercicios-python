b40e9b02c578b1b07c3f3d3f29d6665769aab547
Leo Porto <leonardoreisporto@gmail.com>
2024-08-21 07:17:22 -0300

    feat: exercicios

---
+++ Listas/ex21.py
@@ -0,0 +1,7 @@
+# 21. **Ordenar Lista**: Receba uma lista de números e ordene-os em ordem crescente.
+numeros = [10, 0, 100, -78, 15]
+
+numeros_ordenados = sorted(numeros)
+
+print(numeros)
+print(numeros_ordenados)
---
+++ Listas/ex22.py
@@ -0,0 +1,6 @@
+# 22. **Remover Duplicatas**: Remova valores duplicados de uma lista.
+lista = [0, 0, 1, 1, 2, 2, 3, 4]
+sem_duplicatas = list(set(lista))
+
+print(lista)
+print(sem_duplicatas)
