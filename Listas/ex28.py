0f84af3dad4101c5077bda27e40a69a6d1b44c96
Leo Porto <leonardoreisporto@gmail.com>
2024-08-24 07:55:47 -0300

    exercicios

---
+++ Listas/ex28.py
@@ -0,0 +1,10 @@
+# 28. **Média de Elementos**: Calcule a média dos elementos de uma lista.
+lista = [10, 20, 30]
+soma = 0
+
+for i in lista:
+    soma += i
+
+media = soma / len(lista)
+
+print(media)
