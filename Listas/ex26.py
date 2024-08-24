85bc7e456e266a777a04ae55f9fbd95677141157
Leo Porto <leonardoreisporto@gmail.com>
2024-08-24 07:46:57 -0300

    exercicios

---
+++ Listas/ex26.py
@@ -0,0 +1,7 @@
+# 26. **Encontrar o Maior e Menor Elemento**: Identifique o maior e
+# o menor elemento de uma lista.
+lista = [0, -100, 100, 12, 32, 100, 0, 12]
+menor = min(lista)
+maior = max(lista)
+
+print(f"Maior: {maior}\nMenor: {menor}")
