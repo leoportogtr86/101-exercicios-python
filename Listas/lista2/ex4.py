fb8b01d425300db7179b0cfea507f07aaccef579
Leo Porto <leonardoreisporto@gmail.com>
2024-09-06 15:26:36 -0300

    exercicios

---
+++ Listas/lista2/ex4.py
@@ -0,0 +1,8 @@
+# ### **Exercício 4: Acessando um Índice Fora do Alcance**
+#
+# Crie uma lista chamada `animais` com os elementos `"gato"`, `"cachorro"`, `"papagaio"`.
+# Tente acessar o elemento no índice `5` e observe o erro gerado.
+
+animais = ["gato", "cachorro", "papagaio"]
+
+print(animais[4])
---
+++ Listas/lista2/ex5.py
@@ -0,0 +1,8 @@
+# ### **Exercício 5: Fatiamento Simples**
+#
+# Usando a lista `numeros` criada no exercício 3, faça um fatiamento para
+# obter os três primeiros elementos da lista e imprima o resultado.
+
+numeros = [5, 10, 15, 20, 25]
+
+print(numeros[0:3])
