ec465247bdeef623c29fed4614596cfc0ee96dff
Leo Porto <leonardoreisporto@gmail.com>
2024-09-06 10:54:13 -0300

    exercicios

---
+++ Listas/lista1/ex1.py
@@ -0,0 +1,7 @@
+### **Exercício 1: Criação de Lista Simples**
+# Crie uma lista chamada `numeros` que contenha os números
+# de 1 a 5 e, em seguida, imprima essa lista.
+
+numeros = [1, 2, 3, 4, 5]
+
+print(numeros)
---
+++ Listas/lista1/ex2.py
@@ -0,0 +1,8 @@
+### **Exercício 2: Lista Vazia e Adição de Elementos**
+# Crie uma lista vazia chamada `minha_lista` e adicione os seguintes elementos:
+# `"Python"`, `42`, `True`. Imprima a lista
+# ao final.
+
+minha_lista = ["Python", 42, True]
+
+print(minha_lista)
