bdcab7a4add97f2080ccc9d3a5ef5104227d1403
Leo Porto <leonardoreisporto@gmail.com>
2024-09-06 19:58:20 -0300

    exercicios

---
+++ Listas/lista2/ex6.py
@@ -0,0 +1,7 @@
+# ### **Exercício 6: Fatiamento com Índices Negativos**
+#
+# Usando a mesma lista `numeros`, faça um fatiamento para obter os dois
+# últimos elementos usando índices negativos e imprima o resultado.
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(numeros[-2:])
---
+++ Listas/lista2/ex7.py
@@ -0,0 +1,8 @@
+# ### **Exercício 7: Iterando com Índices**
+#
+# Crie uma lista chamada `frutas` com os elementos `"maçã"`, `"banana"`, `"laranja"`, `"uva"`.
+# Use um loop `for` para imprimir cada elemento junto com seu índice correspondente.
+frutas = ["maçã", "banana", "laranja", "uva"]
+
+for fruta in frutas:
+    print(fruta)
