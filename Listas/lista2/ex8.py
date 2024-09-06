86cd044871303edd2e8ce7bae35d6f242b2c24b2
Leo Porto <leonardoreisporto@gmail.com>
2024-09-06 20:02:39 -0300

    exercicios

---
+++ Listas/lista2/ex8.py
@@ -0,0 +1,10 @@
+### **Exercício 8: Somando Dois Elementos Usando Índices**
+
+# Crie uma lista chamada `valores` com os elementos `[100, 200, 300, 400, 500]`.
+# Some o segundo e o quarto elementos da lista e imprima o resultado.
+
+valores = [100, 200, 300, 400, 500]
+
+res = valores[1] + valores[3]
+
+print(res)
---
+++ Listas/lista2/ex9.py
@@ -0,0 +1,11 @@
+### **Exercício 9: Modificando Vários Elementos com Fatiamento**
+
+# Usando a lista `valores` do exercício anterior, altere os dois últimos
+# elementos para `[600, 700]` usando fatiamento e imprima a lista atualizada.
+
+valores = [100, 200, 300, 400, 500]
+
+valores[-1] = 700
+valores[-2] = 600
+
+print(valores)
